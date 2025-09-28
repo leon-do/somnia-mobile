4749b8150435d309986e0c35c8c7c3903d64ecfb
Leon <leon@0sum.io>
2025-09-27 21:30:02 -0400

    reown working

---
+++ app/_layout.tsx
@@ -0,0 +1,96 @@
+import "@walletconnect/react-native-compat";
+import {
+  AppKit,
+  createAppKit,
+  defaultWagmiConfig,
+} from "@reown/appkit-wagmi-react-native";
+import { authConnector } from "@reown/appkit-auth-wagmi-react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { mainnet, monadTestnet } from "@wagmi/core/chains";
+import { WagmiProvider } from "wagmi";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { View } from "react-native";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://dashboard.reown.com
+const projectId = "b8e39dfb697ba26ac5a77a4b29b35604"; // "3f92e051b3075aa241098fde5e040e44";
+
+// 2. Create config
+const metadata = {
+  name: "AppKit RN",
+  description: "AppKit RN Example",
+  url: "https://reown.com/appkit",
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
+
+const auth = authConnector({ projectId, metadata });
+
+const chains = [mainnet, monadTestnet] as const;
+
+const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  extraConnectors: [auth],
+});
+
+// 3. Create modal
+createAppKit({
+  projectId,
+  metadata,
+  chainImages: {
+    10143: "https://files.svgcdn.io/token-branded/monad.png", // Monad Testnet
+  },
+  wagmiConfig,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    KHTeka: require("../assets/fonts/KHTeka-Regular.otf"),
+    KHTekaMedium: require("../assets/fonts/KHTeka-Medium.otf"),
+    KHTekaMono: require("../assets/fonts/KHTekaMono-Regular.otf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          {/* This is a workaround for the Android modal issue. https://github.com/expo/expo/issues/32991#issuecomment-2489620459 */}
+          <View style={{ position: "absolute", height: "100%", width: "100%" }}>
+            <AppKit />
+          </View>
+          <StatusBar style="auto" />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ThemeProvider>
+  );
+}
